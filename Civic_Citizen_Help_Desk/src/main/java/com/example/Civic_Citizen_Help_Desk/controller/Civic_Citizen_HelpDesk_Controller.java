5b496f61830d80cd51b3be42d14efd4f2e28f0b2
akash <kumbharakash978@gmail.com>
2025-10-15 15:07:07 +0530

    Validation

--- Civic_Citizen_Help_Desk/src/main/java/com/example/Civic_Citizen_Help_Desk/controller/Civic_Citizen_HelpDesk_Controller.java
+++ Civic_Citizen_Help_Desk/src/main/java/com/example/Civic_Citizen_Help_Desk/controller/Civic_Citizen_HelpDesk_Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,11 +35,31 @@ public class Civic_Citizen_HelpDesk_Controller {
 		
 	}
 	
-	 //login
+	
+	  //login
 	@PostMapping("/login")
 	public ResponseEntity<Boolean> login(@RequestBody Civic_HelpDeskEntity user) {
 	    boolean isValid = service.login_user(user); 
 	    return ResponseEntity.ok(isValid); 
 	}
 
+	/*
+	 * @PostMapping("/login") public ResponseEntity<Boolean> login(@RequestBody
+	 * Civic_HelpDeskEntity civic_helpdeskentity) { boolean isValid =
+	 * service.login_user(civic_helpdeskentity); return ResponseEntity.ok(isValid);
+	 * }
+	 */
+	
+    @PostMapping("/checkEmail")
+	public boolean findbyemail(@PathVariable String email) {
+		return service.findbyemail(email);
+	}
+     @PostMapping("/checkMobile")
+	public boolean findbymobile(@PathVariable String mobile) {
+		return service.findbymobile(mobile);
+	}
+     @PostMapping("/checkAadhar")
+	public boolean findbyaadhar(@PathVariable String aadhar) {
+		return service.findbyaadhar(aadhar);
+	}
 }
